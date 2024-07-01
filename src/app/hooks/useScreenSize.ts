0a20c427198b7452cd7e36e740030ba7707b24cf
Lukman Rambe <lukmanrambe1945@gmail.com>
2024-07-01 07:28:18 +0700

    refactor(useScreenSize): initial value

--- src/app/hooks/useScreenSize.ts
+++ src/app/hooks/useScreenSize.ts
@@ -4,27 +4,28 @@ const useScreenSize = () => {
   const [screenSize, setScreenSize] = useState<{
     width: number;
     height: number;
-  }>({
-    width: 0,
-    height: 0,
-  });
+  }>(() =>
+    typeof window !== 'undefined'
+      ? { width: window.innerWidth, height: window.innerHeight }
+      : { width: 1025, height: 1025 }
+  );
 
   useEffect(() => {
     const handleResize = () => {
-      if (typeof window !== undefined) {
+      if (typeof window !== 'undefined') {
         setScreenSize({
           width: window.innerWidth,
           height: window.innerHeight,
         });
       }
     };
-    if (typeof window !== undefined) {
+    if (typeof window !== 'undefined') {
       window.addEventListener('resize', handleResize);
     }
 
     // Clean up the event listener when the component unmounts
     return () => {
-      if (typeof window !== undefined) {
+      if (typeof window !== 'undefined') {
         window.removeEventListener('resize', handleResize);
       }
     };
